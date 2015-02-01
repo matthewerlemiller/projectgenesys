aa2196944f3c4fdc15461976f18087390e7026e2
Austen Payan <austenpayan@gmail.com>
2015-01-31 16:04:27 -0800

    Create structure for members page

---
+++ app/database/seeds/LeaderTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+class LeaderTableSeeder extends Seeder {
+
+	public function run() {
+
+		DB::table('leaders')->delete();
+
+		$faker = Faker\Factory::create();
+
+		foreach(range(1, 25) as $index) {
+
+			$leader = Leader::create([
+
+				'LeaderFirstName' => $faker->firstNameMale,
+				'LeaderLastName' => $faker->lastName,
+				'Email' => $faker->email,
+
+			]);
+
+
+		}
+
+	}
+	
+
+}
