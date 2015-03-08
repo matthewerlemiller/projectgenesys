5ffda7da3a33fd55df49fed18d3934d768af4cea
Austen Payan <austenpayan@gmail.com>
2015-03-07 17:18:02 -0800

    fix header issue

--- public/js/pages/dashboard.js
+++ public/js/pages/dashboard.js
@@ -9,57 +9,128 @@ function sizeResults() {
 	$results.height(resultSize);
 }
 
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+ 
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+ 
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
 //handles the clicking of the hamburger menu
 
 
 //function to animate the header out of view when scrolling down and back into view when scrolling up. needs to be replaced.
-function stickyHeader(){
+// function stickyHeader(){
 
-	var $header = $('.nav-container');
-	$(window).scroll(function(){
+// 	var $header = $('.nav-container');
+// 	$(window).scroll(function(){
 
-		var newScrollPosition = $(window).scrollTop();
+// 		var newScrollPosition = $(window).scrollTop();
 
-		if (newScrollPosition > 150) {
+// 		if (newScrollPosition > 150) {
 
-			$header.css('transition', '.5s');
-			var scrollDiff = Math.abs(newScrollPosition - scrollPosition);
+// 			$header.css('transition', '.5s');
+// 			var scrollDiff = Math.abs(newScrollPosition - scrollPosition);
 
-			if (newScrollPosition > scrollPosition) {
-				if (scrollDiff > 10){
-					scrollPosition = newScrollPosition;
-					$header.css('display','none');
-					$header.css('top','-70px');
-					setTimeout(function(){
-						$header.css('position','fixed');
-						$header.css('display','block');
-					},10);
-				}
+// 			if (newScrollPosition > scrollPosition) {
+// 				if (scrollDiff > 10){
+// 					scrollPosition = newScrollPosition;
+// 					$header.css('display','none');
+// 					$header.css('top','-70px');
+// 					setTimeout(function(){
+// 						$header.css('position','fixed');
+// 						$header.css('display','block');
+// 					},10);
+// 				}
+// 			}
+
+// 			else {
+// 				if (scrollDiff > 25){
+// 					scrollPosition = newScrollPosition
+// 					$header.css('top','0px')
+// 					setTimeout(function(){$header.css('position','fixed');},10);
+// 				}
+// 			}
+// 		}
+
+// 		else if (newScrollPosition == 0){
+// 			$('.search-container').css('position','static')
+// 			// $header.css('transition', '0s');
+// 		}
+// 	});
+// }
+
+// //initializing some global variables
+
+// var scrollPosition = 0;
+
+var Header = {
+
+	init : function() {
+
+		requestAnimationFrame(Header.stickify);
+
+	},
+
+	element : $(".nav-container"),
+
+	previousDistance : 0,
+
+	stickify : function() {
+
+		var distance = $(document).scrollTop();
+
+		if(distance > 150) {
+
+			if(distance > Header.previousDistance && distance > Header.previousDistance + 10) {
+
+				Header.element.addClass('hidden');
+				
+
+			} else if (distance < Header.previousDistance && distance < Header.previousDistance + 10) {
+
+				Header.element.removeClass('hidden');
+				
+
 			}
 
-			else {
-				if (scrollDiff > 25){
-					scrollPosition = newScrollPosition
-					$header.css('top','0px')
-					setTimeout(function(){$header.css('position','fixed');},10);
-				}
-			}
+			Header.previousDistance = distance;
+
+		} else {
+
+			
+			Header.element.removeClass('hidden');
+
 		}
 
-		else if (newScrollPosition == 0){
-			$('.search-container').css('position','static')
-			// $header.css('transition', '0s');
-		}
-	});
+		requestAnimationFrame(Header.stickify);
+
+	}
+
 }
 
-//initializing some global variables
-
-var scrollPosition = 0;
-
+Header.init();
 
 //calling functions
 
 sizeResults();
 
-stickyHeader();
+// stickyHeader();
