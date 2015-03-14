ef61e92b18f382f85387d1e28ee19954f9c695ce
Austen Payan <austenpayan@gmail.com>
2015-03-14 09:53:33 -0700

    temporarily update leader table seeder

--- app/database/seeds/LeaderTableSeeder.php
+++ app/database/seeds/LeaderTableSeeder.php
@@ -6,19 +6,25 @@ class LeaderTableSeeder extends Seeder {
 
 		DB::table('leaders')->delete();
 
-		$faker = Faker\Factory::create();
+		// $faker = Faker\Factory::create();
 
-		foreach(range(1, 25) as $index) {
+		// foreach(range(1, 25) as $index) {
 
-			$leader = Leader::create([
+		// 	$leader = Leader::create([
 
-				'LeaderFirstName' => $faker->firstNameMale,
-				'LeaderLastName' => $faker->lastName,
-				'Email' => $faker->email,
+		// 		'LeaderFirstName' => $faker->firstNameMale,
+		// 		'LeaderLastName' => $faker->lastName,
+		// 		'Email' => $faker->email,
 
-			]);
+		// 	]);
 
-		}
+		// }
+
+		Leader::create(['LeaderFirstName' => 'Jeremy', 'LeaderLastName' => 'Miller', 'Email' => 'random@gmail.com']);
+		Leader::create(['LeaderFirstName' => 'David', 'LeaderLastName' => 'Matranga', 'Email' => 'random@gmail.com']);
+		Leader::create(['LeaderFirstName' => 'Josh', 'LeaderLastName' => 'Simmons', 'Email' => 'random@gmail.com']);
+		Leader::create(['LeaderFirstName' => 'Mark', 'LeaderLastName' => 'Hoffman', 'Email' => 'random@gmail.com']);
+		Leader::create(['LeaderFirstName' => 'John', 'LeaderLastName' => 'Doe', 'Email' => 'random@gmail.com']);
 		
 	}
 	
