a82dd3e1e33ed441dab1e8564eaf00ca746667d1
Austen Payan <austenpayan@gmail.com>
2016-05-10 20:05:35 -0700

    add center name to dashboard view

--- app/views/dashboard.blade.php
+++ app/views/dashboard.blade.php
@@ -10,7 +10,7 @@
 		<div class="spacer"></div>
 
 		<div class="Card results-wrapper" ng-controller="DashboardController">
-			<div class="Card-title">Checked In Members <span class="dashboard-counter">@{{ checkInLogs.length }}</span></div>
+			<div class="Card-title">{{ Auth::user()->name }} - <span class="dashboard-counter">@{{ checkInLogs.length }}</span> Checked In</div>
 			<div class="Card-content" ng-cloak>
 				<a href="member/@{{ checkInLog.member.id }}" ng-repeat="checkInLog in checkInLogs">
 					<div class="result" ng-class="{ 'left-result' : $index % 3 === 0, 'right-result' : ($index + 1) % 3 === 0 }">
