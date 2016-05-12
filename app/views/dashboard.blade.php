6b084b31a3e50f2d5962c961e7d7ff6a804884cf
Austen Payan <austenpayan@gmail.com>
2016-05-11 19:49:32 -0700

    add indicators for members with no permision slip

--- app/views/dashboard.blade.php
+++ app/views/dashboard.blade.php
@@ -2,25 +2,31 @@
 
 @section('content')
 
-	<div class="main-wrapper">
-		<div class="add-new-button">
-			<a href="{{ route('member.create') }}" class="create-new"><i class="fa fa-plus"></i></a>
-		</div>
+    <div class="main-wrapper">
+        <div class="add-new-button">
+            <a href="{{ route('member.create') }}" class="create-new"><i class="fa fa-plus"></i></a>
+        </div>
 
-		<div class="spacer"></div>
+        <div class="spacer"></div>
 
-		<div class="Card results-wrapper" ng-controller="DashboardController">
-			<div class="Card-title">{{ Auth::user()->name }} - <span class="dashboard-counter">@{{ checkInLogs.length }}</span> Checked In</div>
-			<div class="Card-content" ng-cloak>
-				<a href="member/@{{ checkInLog.member.id }}" ng-repeat="checkInLog in checkInLogs">
-					<div class="result" ng-class="{ 'left-result' : $index % 3 === 0, 'right-result' : ($index + 1) % 3 === 0 }">
-						<div class="Flair Flair--orange Flair-top" ng-if="checkInLog.member.leadsJrStaff" ng-cloak>Jr Staff</div>
-						<div class="pic" back-img="@{{checkInLog.member.image}}"></div>
-						<p class="name">@{{ checkInLog.member.firstName }} @{{ checkInLog.member.lastName }}</p>
-					</div>
-				</a>
-			</div>
-		</div>
-	</div>
+        <div class="Card results-wrapper" ng-controller="DashboardController">
+            <div class="Card-title">{{ Auth::user()->name }} - <span class="dashboard-counter">@{{ checkInLogs.length }}</span> Checked In</div>
+            <div class="Card-content" ng-cloak>
+                <a href="member/@{{ checkInLog.member.id }}" ng-repeat="checkInLog in checkInLogs">
+                    <div class="result" ng-class="{ 'left-result' : $index % 3 === 0, 'right-result' : ($index + 1) % 3 === 0 }">
+                        <div class="Flair Flair--orange Flair-top" ng-if="checkInLog.member.leadsJrStaff" ng-cloak>Jr Staff</div>
+                        <div class="pic" back-img="@{{checkInLog.member.image}}">
+                            <div class="indicators">
+                                <div class="Indicator Indicator--small Indicator--negative" ng-show="!checkInLog.member.permissionSlip">
+                                    <i class="Indicator-icon fa fa-file-text"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <p class="name">@{{ checkInLog.member.firstName }} @{{ checkInLog.member.lastName }}</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
 
 @stop
--- app/views/partials/nav.blade.php
+++ app/views/partials/nav.blade.php
@@ -1,70 +1,75 @@
 
 <nav class="offscreen-nav">
-	<ul class="links-container">
-		<a href="{{ route('home') }}"><li class="isFirst">Dashboard</li></a>
-		@if(Auth::user()->admin)
-		<a href=""><li>Members</li></a>
-		<a href="{{ route('admin.leaders') }}"><li>Leaders</li></a>
-		<a href="{{ route('admin.locations') }}"><li>Locations</li></a>
-		<a href=""><li>Suspensions</li></a>
-		<a href=""><li>Metrics</li></a>
-		@endif
-		@if(!Auth::user()->admin)
-		<a href="{{ route('member.create') }}"><li>Add Member</li></a>
-		<a href="{{ route('shift.index') }}"><li>Shift</li></a>
-		@endif
-		<a class="logout" href="{{ route('logout') }}"><li>Sign out</li></a>
-		
-	</ul>
+    <ul class="links-container">
+        <a href="{{ route('home') }}"><li class="isFirst">Dashboard</li></a>
+        @if(Auth::user()->admin)
+        <a href=""><li>Members</li></a>
+        <a href="{{ route('admin.leaders') }}"><li>Leaders</li></a>
+        <a href="{{ route('admin.locations') }}"><li>Locations</li></a>
+        <a href=""><li>Suspensions</li></a>
+        <a href=""><li>Metrics</li></a>
+        @endif
+        @if(!Auth::user()->admin)
+        <a href="{{ route('member.create') }}"><li>Add Member</li></a>
+        <a href="{{ route('shift.index') }}"><li>Shift</li></a>
+        @endif
+        <a class="logout" href="{{ route('logout') }}"><li>Sign out</li></a>
+        
+    </ul>
 </nav>
 
 <input type="checkbox" id="nav-trigger" class="nav-trigger" />
 <label for="nav-trigger">
-	<div class="hamburger">
-		<div class="patty"></div>
-		<div class="patty"></div>
-		<div class="patty"></div>
-	</div>
+    <div class="hamburger">
+        <div class="patty"></div>
+        <div class="patty"></div>
+        <div class="patty"></div>
+    </div>
 
 
 </label>
 
 <!--
-	entire site wrapper so content can be moved to the right when opening menu
-	ends before script tags in master.blade.php
+    entire site wrapper so content can be moved to the right when opening menu
+    ends before script tags in master.blade.php
  -->
 <div class="site-wrap">
-	<div class="header-container" ng-controller="SearchController">
-			<div class="nav-container">
-				<a href="{{ route('home') }}"><img class="logo" src="{{ asset('img/yv-logo.png') }}"></a>
-				<div class="search-field-container">				
-					<input id="member-search-input" class="search-field" type="search" autofocus ng-model="query" off-click="blurSearch()" ng-change="searchForMember()" ng-model-options="{ debounce: 300 }">
-					<i class="fa fa-search search-field-icon"></i>
-				</div>				
-			</div>
-			<div class="results-pane" ng-show="showResults" ng-cloak>
-				<div class="main-wrapper">
-					<!-- Display this if no results -->
-					<div class="no-result-block result-block" ng-if="!results.length">
-						<h4 class="no-result-heading">No Results Were Returned</h4>
-						<p class="no-result-message">Please try another search term or add a new member</p>
-					</div>
-					<!--  End of no results -->
-					<div class="result-block" ng-repeat="member in results">
-						<a href="/member/@{{ member.id }}"><div class="result-image" style="background-image:url(@{{ member.image }})">
-							<div class="Flair Flair-top Flair--orange Flair--small" ng-if="member.leadsJrStaff" ng-cloak>Jr Staff</div>
-						</div></a>
-						<div class="result-block-content">
-							<a href="/member/@{{ member.id }}"><p class="result-name">@{{ member.firstName }} @{{ member.lastName }}</p></a>
-							<status-tag member="member" loaded="showResults"></status-tag>	
-						</div>							
-						<div class="result-checkin" ng-class="{'result-checkin-green' : member.checkedIn }" ng-click="checkIn(member.id, $index)">@{{ member.checkedIn ? 'Checked In!' : 'Check In' }}</div>
-						<div class="clear"></div>
-					</div>
-				</div>
-				<div class="rule"></div>
-			</div>
+    <div class="header-container" ng-controller="SearchController">
+            <div class="nav-container">
+                <a href="{{ route('home') }}"><img class="logo" src="{{ asset('img/yv-logo.png') }}"></a>
+                <div class="search-field-container">                
+                    <input id="member-search-input" class="search-field" type="search" autofocus ng-model="query" off-click="blurSearch()" ng-change="searchForMember()" ng-model-options="{ debounce: 300 }">
+                    <i class="fa fa-search search-field-icon"></i>
+                </div>              
+            </div>
+            <div class="results-pane" ng-show="showResults" ng-cloak>
+                <div class="main-wrapper">
+                    <!-- Display this if no results -->
+                    <div class="no-result-block result-block" ng-if="!results.length">
+                        <h4 class="no-result-heading">No Results Were Returned</h4>
+                        <p class="no-result-message">Please try another search term or add a new member</p>
+                    </div>
+                    <!--  End of no results -->
+                    <div class="result-block" ng-repeat="member in results">
+                        <a href="/member/@{{ member.id }}">
+                            <div class="result-image" style="background-image:url(@{{ member.image }})">
+                                <div class="Flair Flair-top Flair--orange Flair--small" ng-if="member.leadsJrStaff" ng-cloak>Jr Staff</div>                                
+                            </div>
+                        </a>
+                        <div class="result-block-content">
+                            <a href="/member/@{{ member.id }}"><p class="result-name">@{{ member.firstName }} @{{ member.lastName }}</p></a>
+                            <status-tag member="member" loaded="showResults" style="vertical-align:super;"></status-tag> 
+                            <div class="Indicator Indicator--negative Indicator--small" style="vertical-align:bottom;" ng-show="!member.permissionSlip">
+                                <i class="Indicator-icon fa fa-file-text"></i>
+                            </div>
+                        </div>                          
+                        <div class="result-checkin" ng-class="{'result-checkin-green' : member.checkedIn }" ng-click="checkIn(member.id, $index)">@{{ member.checkedIn ? 'Checked In!' : 'Check In' }}</div>
+                        <div class="clear"></div>
+                    </div>
+                </div>
+                <div class="rule"></div>
+            </div>
 
-	</div>
+    </div>
 
 
